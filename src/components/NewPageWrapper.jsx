10b40f44181cbb730b7becd011fd19ba4160c90f
Paidlife8 <paidlife8@gmail.com>
2025-06-05 20:20:24 -0700

    update

--- src/components/NewPageWrapper.jsx
+++ src/components/NewPageWrapper.jsx
@@ -7,7 +7,7 @@ const NewPageWrapper = ({ children }) => {
       <head>
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge" />
-        <title>Finbest - Banking and Finance HTML Template </title>
+        <title>Wes Credit </title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
